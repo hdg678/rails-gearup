df4570f90f43991d402d7089fa0f3353cc347758
Hunter Gregory <hdg678@gmail.com>
2016-11-14 17:04:52 +0000

    updating frame with controllers and views

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,4 +1,6 @@
-class UsersController < ApplicationController
+class EquipmentsController < ApplicationController
+  def index
+  end
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+
+  get 'bookings/show'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/edit'
+
+  get 'bookings/update'
+
+  get 'bookings/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'equipments/index'
+
+  get 'equipments/show'
+
+  get 'equipments/new'
+
+  get 'equipments/create'
+
+  get 'equipments/edit'
+
+  get 'equipments/update'
+
+  get 'equipments/destroy'
+
   root to: 'pages#home'
   resources :users do
     resources :equipments
