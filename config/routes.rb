73717313b83aaf328470afb48aeb90da0fb6121b
Hunter Gregory <hdg678@gmail.com>
2016-11-14 18:56:01 +0000

    adding devise logging

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
 
-  get 'bookings/show'
-
-  get 'bookings/new'
-
-  get 'bookings/create'
-
-  get 'bookings/edit'
-
-  get 'bookings/update'
-
-  get 'bookings/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'equipments/index'
-
-  get 'equipments/show'
-
-  get 'equipments/new'
-
-  get 'equipments/create'
-
-  get 'equipments/edit'
-
-  get 'equipments/update'
-
-  get 'equipments/destroy'
-
+  devise_for :users
   root to: 'pages#home'
+  get '/dashboard', to: 'pages#dashboard'
+
   resources :users do
     resources :equipments
     resources :bookings
