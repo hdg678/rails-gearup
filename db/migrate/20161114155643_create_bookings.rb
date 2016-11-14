9e613cc822e0563c9a644a363abe27db3e83d32b
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-14 16:34:39 +0000

    added controllers (bookings, equipement and users) and created routes

--- db/migrate/20161114155643_create_bookings.rb
+++ db/migrate/20161114155643_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
-      t.referencees :user
+      t.references :user
       t.references :equipment, foreign_key: true
       t.date :checkin
       t.date :checkout
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161114155643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "equipment_id"
+    t.date     "checkin"
+    t.date     "checkout"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["equipment_id"], name: "index_bookings_on_equipment_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "equipment", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "description"
+    t.string   "category"
+    t.float    "price"
+    t.string   "instructions"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_equipment_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "equipment"
+  add_foreign_key "equipment", "users"
 end
