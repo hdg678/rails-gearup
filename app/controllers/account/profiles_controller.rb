bd3c88fdb877cf5d0e3876350d5ca71d37f0cda6
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-15 13:38:04 +0000

    edit profile done

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -1,11 +1,14 @@
 class Account::ProfilesController < Account::AccountController
+  before_action :user_params, only: [:update]
   def show
   end
 
   def edit
+
   end
 
   def update
+    @user = current_user.update(user_params)
   end
 
   private
