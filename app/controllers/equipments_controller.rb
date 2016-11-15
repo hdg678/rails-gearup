34106e38330927c4f9cea5e1aa2f1b52aad38f40
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-15 09:57:41 +0000

    user equipments index and show working, problems with new

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,14 +1,18 @@
 class EquipmentsController < ApplicationController
+  before_action :get_equipment, only: [:show, :update, :destroy]
   def index
+    @equipments = Equipment.all
   end
 
   def show
   end
 
   def new
+    @equipment = Equipment.new
   end
 
   def create
+    @equipment = Equipment.create(equipment_params)
   end
 
   def edit
@@ -18,5 +22,15 @@ class EquipmentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def get_equipment
+    @equipment = Equipment.find(params[:id])
+  end
+
+  def equipment_params
+    params.require(:equipment).permit(:title, :description, :category, :price, :instructions)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# categories = Category.create([
+#     {
+#     name: "Cycling"
+#     },
+#     {
+#     name: "Tennis"
+#     },
+#     {
+#     name: "Football"
+#     }
+#     ])
