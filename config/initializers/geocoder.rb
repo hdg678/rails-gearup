5ad5374ab41c8e4af2f7a241a8a6aeb1df326ae7
Hunter Gregory <hdg678@gmail.com>
2016-11-16 11:34:18 +0000

    adding API key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -19,6 +19,9 @@ Geocoder.configure(
   # units: :mi,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 :units => :km,
+:lookup    => :google,
+:api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+:use_https => true,
 
 
 
