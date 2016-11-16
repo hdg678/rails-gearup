3593f31fbeb5388aed6366a8e4806e75806aca3d
Hunter Gregory <hdg678@gmail.com>
2016-11-16 16:14:20 +0000

    adding show page and links

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
-  resources :equipment, only: :index
+  resources :equipment, only: [:index, :show]
 
   # get '/profiles/:id', to: "profiles#show", as: :profile
   # get '/profiles/edit', to: "profiles#edit", as: :edit_profile
